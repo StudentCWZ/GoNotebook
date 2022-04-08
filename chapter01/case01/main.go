ff955641ae3ea0952bc3285ed4375134433300cf
cuiweizhi <330459539@qq.com>
2022-04-08 08:50:41 +0800

    Add a file of chapter01/case01/main.go

---
+++ chapter01/case01/main.go
@@ -0,0 +1,19 @@
+/*
+   @Author: StudentCWZ
+   @Description:
+   @File: main
+   @Software: GoLand
+   @Project: GoNotebook
+   @Date: 2022/4/8 08:44
+*/
+
+package main
+
+/*
+	1. Golang 源码文件使用 UTF-8 编码，对 Unicode 支持良好。每个源文件都属于包的一部分，在文件头部用 package 声明所属包的名称。
+	2. 以 ".go" 作为文件扩展名，语句结束分号会被默认省略，支持 C 样式注释。入口函数 main 没有参数，且必须放在 main 包中。
+*/
+
+func main() {
+	println("hello, world!")
+}
