9342359a831712ee88b05ece187c7f5b4c9c1f84
cuiweizhi <330459539@qq.com>
2022-04-08 09:00:36 +0800

    Add a file of chapter01/case02/main.go

---
+++ chapter01/case02/main.go
@@ -0,0 +1,22 @@
+/*
+   @Author: StudentCWZ
+   @Description:
+   @File: main
+   @Software: GoLand
+   @Project: GoNotebook
+   @Date: 2022/4/8 08:44
+*/
+
+package main
+
+import "fmt"
+
+/*
+	1. 用 import 导入标准库或第三方包。
+	2. 请删除未使用的导入，否则编译器会将其当作错误。
+	3. 可直接运行，或编译为可执行文件。
+*/
+
+func main() {
+	fmt.Println("hello,world!")
+}
